5440e3a95263ef318ba91826c0e2b0a8647b06e2
Umang Jaiswal <mob.dev1-tc@uidai.net.in>
2026-08-18 12:47:44 +0530

    added background color

--- registration/src/main/java/app/gov/uidai/registration/connectivity/ui/NoInternetScreen.kt
+++ registration/src/main/java/app/gov/uidai/registration/connectivity/ui/NoInternetScreen.kt
@@ -1,5 +1,6 @@
 package app.gov.uidai.registration.connectivity.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -25,8 +27,10 @@ fun NoInternetScreen(
 ) {
     Column(
         modifier = modifier
+            .background(color = Color.White)
             .fillMaxSize()
-            .padding(24.dp),
+            .padding(24.dp)
+        ,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
--- registration/src/main/java/app/gov/uidai/registration/connectivity/ui/UnderMaintenanceScreen.kt
+++ registration/src/main/java/app/gov/uidai/registration/connectivity/ui/UnderMaintenanceScreen.kt
@@ -1,5 +1,6 @@
 package app.gov.uidai.registration.connectivity.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -24,6 +26,7 @@ fun UnderMaintenanceScreen(
 ) {
     Column(
         modifier = modifier
+            .background(color = Color.White)
             .fillMaxSize()
             .padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
